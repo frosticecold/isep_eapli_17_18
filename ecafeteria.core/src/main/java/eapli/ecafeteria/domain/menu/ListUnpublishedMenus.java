4809424fcb40f14182bcb7465be8a935916d9279
Raúl Correia [1090657] <1090657@isep.ipp.pt>
2018-04-14 14:34:04 +0100

    [InProgress #23]  * Modified ListUnpublishedMenus

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/menu/ListUnpublishedMenus.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/menu/ListUnpublishedMenus.java
@@ -14,12 +14,28 @@ import java.util.List;
  */
 public class ListUnpublishedMenus {
 
+    /**
+     * Unordered list of critical Menus
+     */
     private List<Menu> criticalMenus;
+    /**
+     * Unordered list of non critical Menus
+     */
     private List<Menu> notCriticalMenus;
 
+    /**
+     * Private empty constructor, this object does not exist empty
+     */
     private ListUnpublishedMenus() {
     }
 
+    /**
+     * Constructor of a ListUnpublishedMenus that receives an Iterable of Menu
+     * It constructos the inner lists according to critical/non critical
+     * deadline menu times
+     *
+     * @param resultOfDB Result of DB query
+     */
     public ListUnpublishedMenus(Iterable<Menu> resultOfDB) {
         criticalMenus = new LinkedList<>();
         notCriticalMenus = new LinkedList<>();
@@ -27,12 +43,22 @@ public class ListUnpublishedMenus {
 
     }
 
+    /**
+     * Adds all results to the respective list
+     *
+     * @param resultOfDB
+     */
     private void addAll(Iterable<Menu> resultOfDB) {
         for (Menu m : resultOfDB) {
             add(m);
         }
     }
 
+    /**
+     * Add a menu to the critical or noncritical lists
+     *
+     * @param menu Menu to add
+     */
     public void add(Menu menu) {
         if (menu.isCritical()) {
             criticalMenus.add(menu);
@@ -41,11 +67,21 @@ public class ListUnpublishedMenus {
         }
     }
 
-    public List<Menu> criticalMenus() {
+    /**
+     * Returns an iterable of critical menus
+     *
+     * @return
+     */
+    public Iterable<Menu> criticalMenus() {
         return criticalMenus;
     }
 
-    public List<Menu> notCriticalMenus() {
+    /**
+     * Returns an iterable of non critical menus
+     *
+     * @return
+     */
+    public Iterable<Menu> notCriticalMenus() {
         return notCriticalMenus;
     }
 }
