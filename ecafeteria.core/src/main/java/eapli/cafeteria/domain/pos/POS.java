9a0c606732c0c0f55065b978ecaa8bfe2c340998
Pedro Coelho <1131485@isep.ipp.pt>
2018-04-14 23:13:32 +0100

    [In progress #40] Implementation of POS aggregate

---
+++ ecafeteria.core/src/main/java/eapli/cafeteria/domain/pos/POS.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.cafeteria.domain.pos;
+
+import eapli.framework.domain.ddd.AggregateRoot;
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ *
+ * @author PedroEmanuelCoelho 1131485@isep.ipp.pt
+ */
+
+//Entity that represents a session start in a point of sale
+
+@Entity
+public class POS implements AggregateRoot<Long>, Serializable{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long idPOS; 
+
+    protected POS () {
+        
+        //for ORM
+    }
+
+    /**
+     * Compares this object with another
+     * @param other
+     * @return 
+     */
+    @Override
+    public boolean sameAs(Object other) {
+        
+        boolean finalResult = false;
+        
+        if(other.getClass() == this.getClass()) { //check if "other" is a POS entity aswell
+           //checks if it has same ID (only atribute of entity that will be unique)
+           POS p = (POS)other;
+           if(p.id() == this.id()) {
+               finalResult = true;
+           }
+        }
+        
+        return finalResult;
+    }
+
+    /**
+     * Returns the id of POS
+     * @return 
+     */
+    @Override
+    public Long id() {
+        return this.idPOS;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public String toString(){ 
+        return "POS id :" + idPOS;
+    }
+ }
