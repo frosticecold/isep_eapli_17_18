0d5a83fc93333e48404933e82c46f76af0e03a19
Pedro Almeida <1161027@isep.ipp.pt>
2018-04-16 23:27:10 +0100

    Create MenuPlan,MenuPlanItem and quantity classes with JPA annotations

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/menuplan/MenuPlanItem.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.domain.menuplan;
+
+import eapli.ecafeteria.domain.meal.Meal;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Version;
+
+@Entity
+public class MenuPlanItem implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "id_menu_plan_item")
+    private long id;
+    
+    @Version
+    private Long version;
+    
+    @OneToOne
+    private Meal currentMeal;
+   
+    @OneToOne
+    private Quantity quantityNumber;
+    
+    protected MenuPlanItem(){
+    
+    }
+
+    public MenuPlanItem(Meal currentMeal, Quantity quantity) {
+        this.currentMeal = currentMeal;
+        this.quantityNumber = quantity;
+    }
+    
+    
+}
