4318d4894b233237a4c37043df99d13e9f7e1745
Pedro Coelho <1131485@isep.ipp.pt>
2018-04-19 21:57:24 +0100

    Update Issue #60 Created service

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/pos/ListingService.java
@@ -0,0 +1,26 @@
+package eapli.ecafeteria.application.pos;
+
+import eapli.ecafeteria.persistence.DishReportingRepository;
+import eapli.ecafeteria.persistence.PersistenceContext;
+import eapli.ecafeteria.reporting.dishes.DishesPerDishType;
+import eapli.framework.application.Controller;
+
+/**
+ *
+ * @author PedroEmanuelCoelho 1131485@isep.ipp.pt
+ */
+public class ListingService implements Controller {
+    
+    public ListingService() {
+        
+    }
+    
+    /** List Avaliable Meals
+     * @return  **/
+    public Iterable<DishesPerDishType> showDishesPerDishType() {
+        
+        DishReportingRepository repo = PersistenceContext.repositories().dishReporting();
+        
+        return repo.dishesPerDishType();
+    }
+}
