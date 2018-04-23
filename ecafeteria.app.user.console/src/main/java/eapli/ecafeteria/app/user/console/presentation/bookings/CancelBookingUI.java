77334136caff229967b8b43c7b47a1fb4afde728
David Camelo <1161294@isep.ipp.pt>
2018-04-23 07:03:54 +0100

    [InProgress #41] * Small fix in ui of cancelBooking

--- ecafeteria.app.user.console/src/main/java/eapli/ecafeteria/app/user/console/presentation/bookings/CancelBookingUI.java
+++ ecafeteria.app.user.console/src/main/java/eapli/ecafeteria/app/user/console/presentation/bookings/CancelBookingUI.java
@@ -36,7 +36,7 @@ public class CancelBookingUI extends AbstractUI{
             selectWidget.show();
             // Select booking
             try {
-                controller.selectBooking(selectWidget.selectedOption());
+                controller.selectBooking(selectWidget.selectedOption() -1);
             } catch (IllegalArgumentException e) {
                 System.out.println("Index not valid!");
             }
@@ -48,10 +48,9 @@ public class CancelBookingUI extends AbstractUI{
                 System.out.println(controller.informRefundValue());
 
                 // Cancel & Confirm
-                System.out.println("Would you like to cancel the booking? [y/n]");
                 String answer = null;
                 do {                
-                    Console.readLine(answer);
+                    answer = Console.readLine("Would you like to cancel the booking? [y/n]");
                 } while (!answer.equals("y") && !answer.equals("n"));
 
                 if(answer.equals("n")){
