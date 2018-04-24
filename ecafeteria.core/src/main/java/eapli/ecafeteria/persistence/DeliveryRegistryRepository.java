32df81de9d5f52afa2035ae920bed6bb0ba51b9c
Pedro Coelho <1131485@isep.ipp.pt>
2018-04-24 14:03:23 +0100

    Created all DeliveryRegistryRepositories

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/persistence/DeliveryRegistryRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.persistence;
+
+import eapli.ecafeteria.domain.pos.DeliveryRegistry;
+import eapli.framework.persistence.repositories.DataRepository;
+
+/**
+ *
+ * @author Peter
+ */
+public interface DeliveryRegistryRepository extends DataRepository<DeliveryRegistry, Long>{
+    
+}
