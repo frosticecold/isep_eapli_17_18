6750e973864c27d6909747a684db768db0442497
Pedro Coelho <1131485@isep.ipp.pt>
2018-04-25 18:00:29 +0100

    Finished creation of POS bootstrapper

--- ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/POSBootstrapper.java
+++ ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/POSBootstrapper.java
@@ -1,8 +1,15 @@
 package eapli.ecafeteria.bootstrapers;
 
+import eapli.ecafeteria.domain.authz.RoleType;
+import eapli.ecafeteria.domain.authz.SystemUser;
+import eapli.ecafeteria.domain.authz.SystemUserBuilder;
+import eapli.ecafeteria.domain.pos.POS;
 import eapli.ecafeteria.persistence.POSRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
 import eapli.framework.actions.Action;
+import eapli.framework.persistence.DataConcurrencyException;
+import eapli.framework.persistence.DataIntegrityViolationException;
+import java.util.logging.Logger;
 
 /**
 *
@@ -13,8 +20,33 @@ public class POSBootstrapper implements Action {
 
     @Override
     public boolean execute() {
+        
         final POSRepository posRepo = PersistenceContext.repositories().posRepository();
         
+        final SystemUserBuilder builder = new SystemUserBuilder();
+        
+        builder.withRole(RoleType.CASHIER);
+        
+        final SystemUser cashier1 = builder.build();
+        
+        final POS pos1 = new POS(cashier1);
+        
+        
         return true;
+    }
+    
+    /**
+     * private method to register on persistence
+     */
+    private void register(POS pos) {
+        
+            try {
+                PersistenceContext.repositories().posRepository().save(pos);
+            } catch (final DataIntegrityViolationException | DataConcurrencyException e) {
+            // ignoring exception. assuming it is just a primary key violation
+            // due to the tentative of inserting a duplicated user
+            Logger.getLogger(ECafeteriaBootstrapper.class.getSimpleName())
+                    .info("EAPLI-DI001: bootstrapping existing record");
+        }
     }
 }
