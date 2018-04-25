bead3d8ff47fb4d1d21302c2279eb05ee6313363
Vítor Brito <1161182@isep.ipp.pt>
2018-04-25 14:36:27 +0100

    Issue #61 Added more verifications

--- ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/kitchen/RegisterBatchUsedInMealUI.java
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/kitchen/RegisterBatchUsedInMealUI.java
@@ -28,9 +28,14 @@ public class RegisterBatchUsedInMealUI extends AbstractUI {
         mealT = MealType.getMealTypeById(mT).name();
 
         Calendar date;
+        int meals;
         do {
             date = Console.readCalendar("Insert date (DD-MM-YYYY):");
-        } while ((this.theController.showMeals(mealT, date) == -1));
+            meals = this.theController.showMeals(mealT, date);
+            if (meals == -1) {
+                System.out.println("There are no meals for the specified date!\n");
+            }
+        } while ((meals == -1));
 
         int state;
         do {
@@ -44,7 +49,7 @@ public class RegisterBatchUsedInMealUI extends AbstractUI {
 
             matAcro = Console.readLine("Input material acronym or exit:");
 
-            if (matAcro.compareTo("exit") == 0) {
+            if (matAcro != null && matAcro.compareTo("exit") == 0) {
                 break;
             }
 
