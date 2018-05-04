93d8c8f1c1e994543f7790a4bf87c10db94cd6a6
André Oliveira <1040862@isep.ipp.pt>
2018-05-04 17:35:38 +0100

    CopyMenu UC creation and Close POS fixes

--- ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/MainMenu.java
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/MainMenu.java
@@ -40,6 +40,7 @@ import eapli.ecafeteria.app.backoffice.console.presentation.kitchen.reporting.Re
 import eapli.ecafeteria.app.backoffice.console.presentation.kitchen.reporting.ReportBookingPerDishUI;
 import eapli.ecafeteria.app.backoffice.console.presentation.kitchen.reporting.ReportBookingPerMealUI;
 import eapli.ecafeteria.app.backoffice.console.presentation.kitchen.reporting.SearchBatchUsageAction;
+import eapli.ecafeteria.app.backoffice.console.presentation.menu.CopyMenuUI;
 import eapli.ecafeteria.app.backoffice.console.presentation.menu.ElaborateOrEditMenuUI;
 import eapli.ecafeteria.app.backoffice.console.presentation.menu.PublishMenuUI;
 import eapli.ecafeteria.application.authz.AuthorizationService;
@@ -351,6 +352,7 @@ public class MainMenu extends AbstractUI {
         final Menu menu = new Menu("Menus >");
 
         menu.add(new MenuItem(MENU_EDIT_CREATE_OPTION, "Edit/Create Menu", () -> new ElaborateOrEditMenuUI().show()));
+        menu.add(new MenuItem(MENU_COPY_OPTION, "Copy Menu", () -> new CopyMenuUI().show()));
         menu.add(new MenuItem(MENU_PUBLISH_OPTION, "Publish Menus", () -> new PublishMenuUI().show()));
         menu.add(new MenuItem(EXIT_OPTION, "Return ", new ReturnAction()));
 
--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/pos/ClosePOSController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/pos/ClosePOSController.java
@@ -10,9 +10,14 @@ import eapli.ecafeteria.domain.dishes.DishType;
 import eapli.ecafeteria.domain.meal.MealType;
 import eapli.ecafeteria.domain.pos.DeliveryMealSession;
 import eapli.ecafeteria.domain.pos.POS;
+import eapli.ecafeteria.persistence.POSRepository;
+import eapli.ecafeteria.persistence.PersistenceContext;
 import eapli.framework.application.Controller;
+import eapli.framework.persistence.DataConcurrencyException;
+import eapli.framework.persistence.DataIntegrityViolationException;
 import java.util.Calendar;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  *
@@ -23,6 +28,8 @@ public class ClosePOSController implements Controller {
     private final ListAvailableMealsService availableMealsService = new ListAvailableMealsService();
     private final POS p = new POS(AuthorizationService.session().authenticatedUser());
     private final DeliveryMealSession d = new DeliveryMealSession(p);
+    private final POSRepository jpaPOS = PersistenceContext.repositories().posRepository();
+    private POS pointofsale;
 
     public void closeSession() {
         AuthorizationService.clearSession();
@@ -37,6 +44,24 @@ public class ClosePOSController implements Controller {
 
     public String checkDate() {
         return d.date().toString();
+    }
+
+    public boolean checkPoSState() {
+        return pointofsale.isClosed();
+    }
+
+    public boolean closeDeliveryMealSession() throws DataConcurrencyException, DataIntegrityViolationException {
+        Optional<POS> POSinDatabase = jpaPOS.findOne(Long.valueOf("1"));
+        if (POSinDatabase.isPresent()) {
+            pointofsale = POSinDatabase.get();
+            if (!checkPoSState()) {
+                pointofsale.changeState();
+                jpaPOS.save(pointofsale);
+            }
+        } else {
+            return false;
+        }
+        return true;
     }
 
     public Map<DishType, Long> listDeliveredMeals(Calendar cal, MealType mt) {
