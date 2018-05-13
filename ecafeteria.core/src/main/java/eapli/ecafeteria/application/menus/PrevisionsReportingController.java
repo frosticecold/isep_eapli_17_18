37e34606f0d7efd167cb21e1ae10b8c7538df395
Pedro Coelho <1131485@isep.ipp.pt>
2018-05-13 18:58:45 +0100

    Created base PrevisionsReportingController

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/menus/PrevisionsReportingController.java
@@ -0,0 +1,14 @@
+package eapli.ecafeteria.application.menus;
+
+import eapli.framework.application.Controller;
+
+/**
+ *
+ * @author PedroEmanuelCoelho 1131485@isep.ipp.pt
+ */
+public class PrevisionsReportingController implements Controller {
+
+    public PrevisionsReportingController() {
+
+    }
+}
