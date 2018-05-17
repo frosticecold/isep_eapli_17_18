9278790b8bba64d685b78564bfdc976ff8a1f748
= <1160900@isep.ipp.pt>
2018-05-17 15:38:25 +0100

    corrections on Create alergen profile UC

--- ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/AlergenProfileBootstrapper.java
+++ ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/AlergenProfileBootstrapper.java
@@ -43,12 +43,12 @@ public class AlergenProfileBootstrapper implements Action {
     }
 
     private void register(String name) throws DataConcurrencyException, DataIntegrityViolationException {
-        final CreateAlergenProfileController controller = new CreateAlergenProfileController();
+        
         final CafeteriaUserRepository userRepo = PersistenceContext.repositories().cafeteriaUsers();
-        final CafeteriaUser user1 = userRepo.findByMecanographicNumber(new MecanographicNumber("900330")).get();
-        AlergenProfile ap = new AlergenProfile(user1);
+        final Optional<CafeteriaUser> user1 = userRepo.findByMecanographicNumber(new MecanographicNumber("900330"));
+        AlergenProfile ap = new AlergenProfile(user1.get());
         alergenPlanRepo.save(ap);
-        controller.save();
+  
 
     }
 
