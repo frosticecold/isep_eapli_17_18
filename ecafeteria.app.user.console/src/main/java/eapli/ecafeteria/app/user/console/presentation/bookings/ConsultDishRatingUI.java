84e6039aea8ad6dda8e3314d61e5efc04eef26f2
JoaoMagalhaes <1160763@isep.ipp.pt>
2018-05-18 21:35:24 +0100

    ConsultDishRating UI and Controller Implementation. Fully Functional.

--- ecafeteria.app.user.console/src/main/java/eapli/ecafeteria/app/user/console/presentation/bookings/ConsultDishRatingUI.java
+++ ecafeteria.app.user.console/src/main/java/eapli/ecafeteria/app/user/console/presentation/bookings/ConsultDishRatingUI.java
@@ -6,11 +6,13 @@ import eapli.ecafeteria.app.user.console.presentation.CafeteriaUserBaseUI;
 import eapli.ecafeteria.application.booking.ConsultDishRatingController;
 import eapli.ecafeteria.application.cafeteriauser.CafeteriaUserBaseController;
 import eapli.ecafeteria.domain.booking.Booking;
+import eapli.ecafeteria.domain.booking.Rating;
 import eapli.ecafeteria.domain.dishes.Dish;
 import eapli.ecafeteria.domain.meal.Meal;
 import eapli.framework.application.Controller;
 import eapli.framework.domain.Designation;
 import eapli.framework.util.Console;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -26,12 +28,13 @@ public class ConsultDishRatingUI extends CafeteriaUserBaseUI {
     
     @Override
     protected boolean doShow() {
-        int j=0,dishNumber;
+        int i=1,dishNumber;
         List<Dish> ld = theController.getDishes();
      
+        /*Presents all the Dishes*/
         for (Dish d : ld) {
-            System.out.println("Dish:" + (j + 1) + "\n" + d.toString());
-            j++;
+            System.out.println("Dish:" + i + "\n" + d.toString());
+            i++;
             System.out.println("***************************************************+");
         }
         
@@ -41,17 +44,37 @@ public class ConsultDishRatingUI extends CafeteriaUserBaseUI {
 
         Dish dish = ld.get(dishNumber - 1);
         
+         /*Gets every Meal from the selected Dish*/
         List<Meal> lm = theController.getMealByDish(dish);
         
-        
         if (!lm.isEmpty()) {
+            List<Rating> totalRatingList = new ArrayList<>();
+            
             for (Meal m : lm) {
                 
-            List<Booking> bookingList = theController.getAllBookingsFromMealThatAreServed(m);
-                
+                /*Gets every Booking from the actual Meal*/
+                List<Booking> bookingList = theController.getAllBookingsFromMealThatAreServed(m);
+            
+                if (!bookingList.isEmpty()) {
+                    
+                    /*Stores every rating from the available bookings*/
+                    for (Booking b : bookingList) {
+                        Rating r = theController.getRatingFromBooking(b);
+                        theController.addRatingToList(r, totalRatingList);
+                    }
+                }
             }
+            /*Calculates the average of ratings*/
+            Double mediatotal = theController.calculateMediaMeal(totalRatingList);
+            
+            /*Apresenta informação*/
+            System.out.println("\nDish Choosen:\n " + dish.toString() + "\n");
+            System.out.println("Número de Ratings: " + totalRatingList.size() + "\n");
+            System.out.println("Media de Ratings: " + mediatotal + "\n");
+            
+            System.out.println("Operação realizada com sucesso!!!");
         } else{
-             System.out.println("Não existem meals com o nome do prato introduzido");
+             System.out.println("Operação realizada com falhou!!!");
         }
         return true;
     }
--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/booking/ConsultDishRatingController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/booking/ConsultDishRatingController.java
@@ -1,6 +1,7 @@
 package eapli.ecafeteria.application.booking;
 
 import eapli.ecafeteria.domain.booking.Booking;
+import eapli.ecafeteria.domain.booking.Rating;
 import eapli.ecafeteria.domain.dishes.Dish;
 import eapli.ecafeteria.domain.meal.Meal;
 import eapli.ecafeteria.persistence.BookingRepository;
@@ -9,9 +10,7 @@ import eapli.ecafeteria.persistence.MealRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
 import eapli.ecafeteria.persistence.RatingRepository;
 import eapli.framework.application.Controller;
-import eapli.framework.domain.Designation;
 import java.util.List;
-import java.util.Optional;
 
 /**
  *
@@ -27,17 +26,66 @@ public class ConsultDishRatingController implements Controller{
     
     private final RatingRepository ratingRepo = PersistenceContext.repositories().rating();
 
+    /**
+     * Gets all Dishes
+     * 
+     * @return 
+     */
     public List<Dish> getDishes(){
         return dishRepo.findListAll(); 
     }
     
+    /**
+     * Gets a list of Meals from a especific Dish passed as a parameter
+     * 
+     * @param dish
+     * @return 
+     */
     public List<Meal> getMealByDish(Dish dish) {
         return mealRepo.getMealByDish(dish);    
     }    
 
+    /**
+     * Gets a list of Bookings from a especific Meal passed as a parameter
+     * 
+     * @param m
+     * @return 
+     */
     public List<Booking> getAllBookingsFromMealThatAreServed(Meal m){
         return bookingRepo.getAllBookingsFromMealThatAreServed(m);
     }
 
+    /**
+     * Gets a rating from a especific Booking passed as a parameter
+     * @param b
+     * @return 
+     */
+    public Rating getRatingFromBooking(Booking b) {
+        return ratingRepo.getRatingFromBooking(b);
+    }
     
+    /**
+     * Adds a rating passed as a parameter to a list passed as a parameter
+     * 
+     * @param r
+     * @param totalRatingList 
+     */
+    public void addRatingToList(Rating r, List<Rating> totalRatingList) {
+        totalRatingList.add(r);
+    }
+
+    /**
+     * Calculates the average of ratings
+     * 
+     * @param totalRatingList
+     * @return 
+     */
+    public double calculateMediaMeal(List<Rating> totalRatingList) {
+        double sum=0;
+        for(Rating r:totalRatingList){
+            sum+=r.getRating();
+        }
+        return sum/totalRatingList.size();
+    }
+
 }
