4e5fd8ec035d6aa8f173888abd8d2d3903b1ca76
Pedro Coelho <1131485@isep.ipp.pt>
2018-05-20 18:34:05 +0100

    Small changes on DeliveryRegistry

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/pos/DeliveryRegistry.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/pos/DeliveryRegistry.java
@@ -4,6 +4,7 @@ import eapli.ecafeteria.domain.booking.Booking;
 import eapli.ecafeteria.domain.cafeteriauser.CafeteriaUser;
 import eapli.framework.domain.ddd.AggregateRoot;
 import java.io.Serializable;
+import java.util.Calendar;
 import javax.persistence.*;
 
 /**
@@ -11,73 +12,79 @@ import javax.persistence.*;
  * @author PedroEmanuelCoelho 1131485@isep.ipp.pt
  */
 @Entity
-public class DeliveryRegistry implements  AggregateRoot<Long>, Serializable {
+public class DeliveryRegistry implements AggregateRoot<Long>, Serializable {
 
     /**
      * identifier of the delivery
      */
     @Id
-    @GeneratedValue(strategy=GenerationType.SEQUENCE)
-    @Column(name="DELIVERY")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "DELIVERYID")
     private Long id;
-    
+
     /**
      * booking
      */
     @OneToOne
-    @JoinColumn(name="BOOKING")
+    @JoinColumn(name = "BOOKING")
     private Booking booking;
-    
+
     /**
      * CLIENT
      */
     @OneToOne
-    @JoinColumn(name="CLIENT")
+    @JoinColumn(name = "CLIENT")
     private CafeteriaUser client;
 
     @ManyToOne
-    @JoinColumn(name="SESSION")
+    @JoinColumn(name = "SESSION")
     private DeliveryMealSession session;
-    
+
+    @Temporal(TemporalType.DATE)
+    private Calendar dateMade;
+
     protected DeliveryRegistry() {
         //for ORM only
     }
-    
+
     public DeliveryRegistry(DeliveryMealSession session, CafeteriaUser client, Booking booking) {
-            this.session = session;
-            this.client = client;
-            this.booking = booking;
+        this.session = session;
+        this.client = client;
+        this.booking = booking;
+        this.dateMade = Calendar.getInstance();
     }
+
     /**
-     * compares this object with another
-     * different id of delivery, different objects
+     * compares this object with another different id of delivery, different
+     * objects
+     *
      * @param other
-     * @return 
+     * @return
      */
     @Override
     public boolean sameAs(Object other) {
-       
-       boolean flag = false;
-        
-       if(other instanceof DeliveryRegistry) {
+
+        boolean flag = false;
+
+        if (other instanceof DeliveryRegistry) {
             flag = true;
-       }
-       
-       return flag;
+        }
+
+        return flag;
     }
 
     @Override
     public boolean is(Long otherId) {
         return AggregateRoot.super.is(otherId); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
+
     /**
      * Return the DeliveryRegistry ID
-     * @return 
+     *
+     * @return
      */
     @Override
     public Long id() {
-       return this.id;
-    } 
+        return this.id;
+    }
 }
