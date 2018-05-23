fe65580ba227046d00fbdbae4e076f4bc8aa5fac
Rui Ribeiro <1150344@isep.ipp.pt>
2018-05-23 15:02:38 +0100

    [Resolve #98] Implementação do padrão Strategy juntamente com o padrão Factory para a validação do número mecanográfico na altura do registo

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/strategy/MechanographicStudentStrategy.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/strategy/MechanographicStudentStrategy.java
@@ -10,7 +10,14 @@ package eapli.ecafeteria.domain.cafeteriauser.strategy;
  * @author Rui Ribeiro <1150344@isep.ipp.pt>
  */
 public class MechanographicStudentStrategy implements MechanographicStrategy {
-
+    
+    
+    /**
+     * Pattern: os dois primeiros digitos são o ano de entrada, os restantes são o numero do aluno
+     * 
+     * @param mechanographicNumber mechanographic number
+     * @return true if matches, false if not
+     */
     @Override
     public boolean isValid(String mechanographicNumber) {
         String pattern = "9\\d\\d{4}|0\\d\\d{4}|1[0-8]\\d{4}";
